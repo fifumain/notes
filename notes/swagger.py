3b491a67b5454c1261787a0e73b46c2f6e0399c0
Filip <pustovoitenkofilip@gmail.com>
2024-11-15 17:01:58 +0200

    upd: swagger auto-docs added

---
+++ notes/swagger.py
@@ -0,0 +1,18 @@
+# core_apps/notes_app/swagger.py
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+# Конфигурация Swagger
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Notes API",
+        default_version="v1",
+        description="Тестове завдання",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(
+            email="pustovoitenkofilip@gmail.com",
+        ),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+)
--- notes/urls.py
+++ notes/urls.py
@@ -2,10 +2,17 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework_simplejwt.views import TokenRefreshView
 
+from .swagger import schema_view
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/register/", include("dj_rest_auth.registration.urls")),
     path("api/", include("dj_rest_auth.urls")),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/", include("core_apps.notes_app.urls")),
+    path(
+        "api/docs/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="swagger-docs",
+    ),
 ]
